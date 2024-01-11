2dfdd6fbf3f37eaf03d49edcf6e77430feabafd9
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-01-11 02:01:40 +0000

    project form done, but still need to add some stuff

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// let pronoun = ['the', 'our'];
+// let adj = ['great', 'big'];
+// let noun = ['jogger', 'racoon'];
+// let domain = ['.com', '.org', '.net', '.us', '.kr'];
+
+// articles
+// adjectives
+// nouns
+// tld (top level domains): .cat, .dog, .lol
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let articles = ["the", "a"];
+  let adjectives = ["big"];
+  let nouns = ["racoon"];
+  let tlds = ["rocks"];
+
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          console.log(`${article}${adjective}${noun}${tld}`);
+        }
+      }
+    }
+  }
 };
+
+// example of what should show:
+// thegreatjogger.com
+// thegreatracoon.com
+// ourgreatjogger.com
+// thebigjogger.com
+// thebigracoon.com
+// ourbigjogger.com
+
+// Hints:
+// You'll need to use nested for loops in order to mix the different values together.
+// Your tools: For loop, string concatenation.
+
+// 😎 Feeling confident?
+// Add domain hacks, for example: instead of lastOfUs.com you can use the .us domain like this: lastOf.us
